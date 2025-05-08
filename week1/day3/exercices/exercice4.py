e97729c5b22f22c0fa0ef0c68a184a1752895592
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2025-05-08 15:07:28 +0100

    Add Exercise 4: Zoo class implementation with methods for adding, selling, and grouping animals

---
+++ week1/day3/exercices/exercice4.py
@@ -0,0 +1,59 @@
+class Zoo:
+    def __init__(self, zoo_name):
+        self.name = zoo_name
+        self.animals = []
+
+    def add_animal(self, new_animal):
+        """Add a new animal to the zoo"""
+        if new_animal not in self.animals:
+            self.animals.append(new_animal)
+
+    def get_animals(self):
+        """Get all animals in the zoo"""
+        for animal in self.animals:
+            print(animal)
+
+    def sell_animal(self, animal_sold):
+        """Sell an animal from the zoo"""
+        if animal_sold in self.animals:
+            self.animals.remove(animal_sold)
+
+    def sort_animals(self):
+        """Sort animals by first letter"""
+        sorted_animals_list = sorted(self.animals)
+        sorted_animals_dict = {}
+
+        for animal in sorted_animals_list:
+            if animal[0] not in sorted_animals_dict:
+                sorted_animals_dict[animal[0]] = [animal]
+            else:
+                sorted_animals_dict[animal[0]].append(animal)
+        return sorted_animals_dict
+
+    def get_group(self):
+        """Get animals grouped by first letter"""
+        sorted_result = self.sort_animals()
+        for key, value in sorted_result.items():
+            print(f"{key}: {value}")
+
+
+# Create an object called new_york_zoo and call all the methods.
+new_york_zoo = Zoo("Wild Park")
+
+new_york_zoo.add_animal("Zebra")
+new_york_zoo.add_animal("Eel")
+new_york_zoo.add_animal("Ape")
+new_york_zoo.add_animal("Baboon")
+new_york_zoo.add_animal("Emu")
+new_york_zoo.add_animal("Bear")
+new_york_zoo.add_animal("Tom")
+new_york_zoo.add_animal("Cougar")
+new_york_zoo.add_animal("Cat")
+
+new_york_zoo.get_animals()
+
+new_york_zoo.sell_animal("Tom")
+
+new_york_zoo.get_group()
+
+
