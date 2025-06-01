35e0aa1cb2a46e84fed862dfced8f589df6ad621
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2025-06-01 16:23:18 +0100

    🌟 Exercise 2 : Shopping List

---
+++ week3/day3/exercises/exercise2.js
@@ -0,0 +1,31 @@
+// Exercise 2 : Shopping List
+const stock = {
+  banana: 6,
+  apple: 0,
+  pear: 12,
+  orange: 32,
+  blueberry: 1,
+};
+
+const prices = {
+  banana: 4,
+  apple: 2,
+  pear: 1,
+  orange: 1.5,
+  blueberry: 10,
+};
+
+const shoppingList = ["banana", "orange", "apple"];
+
+const myBill = () => {
+  let total = 0;
+  for (let item of shoppingList) {
+    if (item in stock && stock[item] > 0) {
+      total += prices[item];
+      stock[item] -= 1;
+    }
+  }
+  return total;
+};
+
+console.log("Total bill:", myBill());
