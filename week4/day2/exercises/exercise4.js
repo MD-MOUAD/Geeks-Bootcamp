dd533e1c39fefa2885d8037d82facb82dadd60da
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2025-06-02 01:27:40 +0100

    🌟 Exercise 4 : Employees

---
+++ week4/day2/exercises/exercise4.js
@@ -0,0 +1,13 @@
+// 🌟 Exercise 1 : Colors
+
+const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+
+colors.forEach((color, index) => {
+  console.log(`${index + 1}# choice is ${color}.`);
+});
+
+if (colors.includes("Violet")) {
+  console.log("Yeah");
+} else {
+  console.log("No...");
+}
