24a4fa1a9f4e0a9298cf74f3be31b744cf49a79c
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2025-06-12 21:58:25 +0100

    Add new exercises for fetching data from APIs

---
+++ week5/day2/exercises/exercise4.js
@@ -0,0 +1,21 @@
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("resolved");
+    }, 2000);
+  });
+}
+
+async function asyncCall() {
+  console.log("calling");
+  let result = await resolveAfter2Seconds();
+  console.log(result);
+}
+
+asyncCall();
+
+// Output:
+
+// log: calling
+// 2 seconds delay
+// log: resolved (after 2 seconds)
