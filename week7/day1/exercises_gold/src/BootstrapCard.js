b384b6194e85c01168ae0c398a61df7cf3eae194
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2025-06-24 15:18:57 +0100

    Exercise XP Gold

---
+++ week7/day1/exercises_gold/src/BootstrapCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const BootstrapCard = ({
+  title,
+  imageUrl,
+  buttonLabel,
+  buttonUrl,
+  description,
+}) => {
+  return (
+    <div className="card m-5" style={{ width: "30rem" }}>
+      <img className="card-img-top" src={imageUrl} alt={title} />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <a href={buttonUrl} className="btn btn-primary">
+          {buttonLabel}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default BootstrapCard;
